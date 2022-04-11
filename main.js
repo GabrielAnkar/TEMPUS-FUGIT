27b7f03e9db51af592ac5a81e700169f23067fe3
Gabriel Ankar <86739799+GabrielAnkar@users.noreply.github.com>
2022-04-11 15:23:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const html = document.documentElement;
+const canvas = document.querySelector('.ScrollScroll');
+const context = canvas.getContext('2d');
+const currentFrame = index => (
+  `SRC/test/${index.toString().padStart(4, '0')}.jpg`
+)
+
+
+const frameCount = 100;
+
+canvas.height = 770;
+
+canvas.width = 1158;
+const img = new Image();
+img.src = currentFrame(1);
+img.onload = function(){
+  context.drawImage(img, 0, 0)
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index)
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () =>{
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop
+  const frameIndex = Math.min(frameCount - 1, Math.floor
+  (scrollFraction * frameCount));
+  
+  requestAnimationFrame( () => updateImage(frameIndex + 1))
+})
